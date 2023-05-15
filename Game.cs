02d6ac9e01e44c2c054cd4240228536010177d1d
Jeremy Goodman <jeremygoodman52@gmail.com>
2023-05-15 14:36:48 -0500

    initial commit

---
+++ Game.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Drawing;
+
+namespace TowerDefense
+{
+    class Game
+    {
+        public static void Main()
+        {
+            Map map = new Map(8, 5);
+
+            Point point = new Point(4, 2);
+
+            Console.WriteLine(point.DistanceTo(5, 5));
+        }
+    }
+}
---
+++ TowerDefense/Point.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TowerDefense
+{
+    public class Point
+    {
+        public readonly int X;
+        public readonly int Y;
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public int DistanceTo(int x, int y)
+        {
+            int xDiff = X - x;
+            int yDiff = Y - y;
+
+            int xDiffSquared = xDiff*xDiff;
+            int yDiffSquared = yDiff*yDiff; 
+
+            return (int)Math.Sqrt(xDiffSquared + yDiffSquared);
+        }
+    }
+}
